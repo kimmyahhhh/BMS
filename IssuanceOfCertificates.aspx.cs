cc8dba483a139bd47deb4eeef8bcaba078296d3c
kimberly gayle <149925741+kimmyahhhh@users.noreply.github.com>
2024-04-21 14:31:11 +0800

    Add files via upload

---
+++ IssuanceOfCertificates.aspx.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyNameSpace
+{
+    public partial class IssuanceOfCertificates : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        { 
+            string purpose = txtPurpose.Text.Trim();
+            if (string.IsNullOrEmpty(purpose))
+            {
+                lblErrorMessage.Visible = true;
+                lblErrorMessage.Text = "Please enter a purpose.";
+                return;
+            }
+
+            Response.Redirect("dashboard.aspx");
+        }
+    }
+}

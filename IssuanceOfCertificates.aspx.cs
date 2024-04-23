9b5f66626327f6448d25be969de43ba97f3959f1
kimberly gayle <149925741+kimmyahhhh@users.noreply.github.com>
2024-04-24 07:32:15 +0800

    Update IssuanceOfCertificates.aspx.cs

--- IssuanceOfCertificates.aspx.cs
+++ IssuanceOfCertificates.aspx.cs
@@ -11,39 +11,6 @@ namespace bms
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string connectionString = "server=localhost;database=bms;uid=root;pwd=";
-            MySqlConnection connection = new MySqlConnection(connectionString);
-
-            string purpose = Request.Form["purpose"];
-            string type = Request.Form["type"];
-
-            if (string.IsNullOrEmpty(purpose) || string.IsNullOrEmpty(type))
-            {
-                Response.Write("Please fill in all fields.");
-            return;
-            }
-
-            try
-            {
-            string query = "INSERT INTO certificates (purpose, type) VALUES (@purpose, @type)";
-            MySqlCommand command = new MySqlCommand(query, connection);
-            command.Parameters.AddWithValue("@purpose", purpose);
-            command.Parameters.AddWithValue("@type", type);
-            connection.Open();
-            int rowsAffected = command.ExecuteNonQuery();
-            if (rowsAffected > 0)
-            {
-                Response.Write("Certificate issued successfully.");
-            }
-            else
-            {
-                Response.Write("Error issuing certificate.");
-            }
-            }
-            catch (Exception ex)
-            {
-            Response.Write("Error: " + ex.Message);
-            }
         }
 
         protected void btnSubmit_Click(object sender, EventArgs e)

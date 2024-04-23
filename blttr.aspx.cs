a364b5126e848889e1ba1ce0d1813643cbbdb9d6
Chalve <164105910+chalve23@users.noreply.github.com>
2024-04-23 15:47:49 +0800

    Add files via upload

---
+++ blttr.aspx.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace bms
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void search_TextChanged(object sender, EventArgs e)
+        {
+            // Search functionality here
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            // Submit button functionality here
+        }
+    }
+}

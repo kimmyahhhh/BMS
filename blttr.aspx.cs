5788554cd24cd1d820c2682346e9db4174fafe36
RAINE <164106280+K-LORRAINE@users.noreply.github.com>
2024-05-22 21:26:29 +0800

    Blotter report. Aspx

--- blttr.aspx.cs
+++ blttr.aspx.cs
@@ -1,27 +1,58 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Data;
 
-namespace bms
+namespace blotter
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        MySqlConnection con = new MySqlConnection("datasource=localhost;port=3306;Database = bms-1;username=root;passwoerd=; convert zero datetime= True");
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            filldview();
         }
-
-        protected void search_TextChanged(object sender, EventArgs e)
+        void filldview()
         {
-            // Search functionality here
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            MySqlCommand cmd = new MySqlCommand("Select * From blotter", con);
+            MySqlDataAdapter adap = new MySqlDataAdapter(cmd);
+            DataTable dtb = new DataTable();
+            adap.Fill(dtb);
+            con.Close();
+
+            GridView1.DataSource = dtb;
+            GridView1.DataBind();
         }
 
-        protected void btnSubmit_Click(object sender, EventArgs e)
+        protected void submitButton_Click(object sender, EventArgs e)
         {
-            // Submit button functionality here
+           
         }
+
+
+            protected void btnShowModal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        protected void OnPageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            this.filldview();
+        }
+
     }
 }
+
+       
+       
